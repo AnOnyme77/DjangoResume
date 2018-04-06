98a8c274b88554bd07ca1e5376530d888e42b53f
AnOnyme77 <anonyme77@protonmail.com>
2018-04-07 00:24:05 +0200

    test

--- Spidermind/settings.py
+++ Spidermind/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vbpf212o4l35d&^gfg9d&w1=u6ie9i_c52mc@wy5@x9ou^5-6a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1',
                  "99d60741.ngrok.io",
