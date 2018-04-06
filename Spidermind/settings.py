116b465a5cd86feea5fea8d4dbecb715d131ce89
AnOnyme77 <anonyme77@protonmail.com>
2018-04-07 00:53:31 +0200

    test

--- Spidermind/settings.py
+++ Spidermind/settings.py
@@ -127,5 +127,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
